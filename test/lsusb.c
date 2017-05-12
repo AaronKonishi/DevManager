b71f765b53c06bb5fad8c89c8174ce776e49834f
Aaron Konishi <zhangqiyin@hangsheng.com.cn>
2017-05-12 15:35:36 +0800

    add print and judge to lsusb.c, avoid error print.

--- test/lsusb.c
+++ test/lsusb.c
@@ -25,6 +25,7 @@ int main(void)
 	libusb_device **devs;  // retrieve a list of device
 	libusb_device_handle *dev_handle;  // a device handle
 	libusb_context *ctx = NULL; // a libusb session
+	const struct libusb_version * version;
 
 	ret = libusb_init(&ctx);
 	if(ret < 0)
@@ -34,6 +35,10 @@ int main(void)
 	}
 
 	libusb_set_debug(ctx, LIBUSB_LOG_LEVEL_INFO);  // set verbosity level to 3, as suggested in the documentation
+
+	version = libusb_get_version();
+	printf("libusb version:%d.%d.%d.%d rc:%s:  %s\n",version->major, version->minor, version->micro, version->nano,
+        version->rc, version->describe);
 
 	cnt = libusb_get_device_list(ctx, &devs);  // get the list of devices
 	if(cnt < 0)
@@ -45,18 +50,23 @@ int main(void)
 
 	dev_handle = libusb_open_device_with_vid_pid(ctx, VID, PID); // these are vendorID and ProductID I found for my HP
 	if(dev_handle == NULL)
+	{
+		libusb_exit(ctx); // needs to be called to end
 		perror("Cannot open device\n");
-	else
-		printf("Device opened.\n");
+		return -1;
+	}
+
+	printf("Device opened.\n");
 	libusb_free_device_list(devs, 1);
 
 	if(libusb_kernel_driver_active(dev_handle, 0) == 1) // find out if kernel driver is attached
 	{
-		printf("Kernel Driver Active");
+		printf("Kernel Driver Active\n");
 		if(libusb_detach_kernel_driver(dev_handle, 0) == 0) // detach it
 			printf("Kernel Driver Detached\n");
 	}
 
+	printf("libusb_kernel_driver_active\n");
 	ret = libusb_claim_interface(dev_handle, 0); // claim interface 0(the first) of device
 	if(ret < 0)
 	{
